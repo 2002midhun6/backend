14c0f1236454fe87b701482e054e56e28b4f93b8
Midhun <midhung@Midhuns-MacBook-Air.local>
2025-09-30 09:05:39 +0530

    extra content for the main is added

--- backend/utils.py
+++ backend/utils.py
@@ -4,11 +4,31 @@ import random
 from django.utils import timezone
 
 def send_otp_email(user):
-    subject = 'Your OTP for verification'
+    subject = 'Your OTP for Account Verification'
     
-    message = f'thank you ...Your OTP for account verification is: {user.otp}'
+    message = f'''Dear {user.username or "User"},
+
+Thank you for signing up!
+
+Your One-Time Password (OTP) for account verification is: {user.otp}
+
+This OTP is valid for 10 minutes and can only be used once.
+
+For your security:
+- Do not share this OTP with anyone
+- We will never ask for your OTP via phone or email
+- If you didn't request this verification, please ignore this email
+
+If you need assistance, please contact our support team.
+
+Best regards,
+{settings.SITE_NAME or "The Team"}
+
+---
+This is an automated message, please do not reply to this email.
+'''
+    
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [user.email]
     
-    # Send only one email
     send_mail(subject, message, from_email, recipient_list)
